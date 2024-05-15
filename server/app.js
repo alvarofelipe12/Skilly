d7287fcaad56d9fae8c8dc083e8c75d66153ad82
Alvaro Felipe Garcia Mendez <alvarofelipe12@gmail.com>
2024-05-15 14:12:09 -0400

    proxy for CORS handling

--- server/app.js
+++ server/app.js
@@ -30,6 +30,28 @@ app.get('/user/:username', (req, res) => {
     });
   }
 });
+app.get('/cors/:url', (req, res) => {
+  console.log(req.params);
+  if (req.params.url) {
+    https.get(`${req.params.url}`, (resp) => {
+      let data = '';
+
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        // console.log("=>>data", data);
+        res.send(JSON.parse(data));
+      });
+
+    }).on("error", (err) => {
+      res.send("Error: " + err.message);
+    });
+  }
+});
 app.get('/', (req, res) => {
   res.send("Welcome to CORS server! 😁")
 })
